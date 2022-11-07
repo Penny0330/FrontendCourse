ee2d55285a3c99824fa032cf3009c090447eea85
Penny Lin <ox1566@gmail.com>
2022-11-07 17:11:44 +0800

    update max_of_two_numbers.js

--- Flow_Control_Assigment/max_of_two_numbers.js
+++ Flow_Control_Assigment/max_of_two_numbers.js
@@ -5,8 +5,8 @@ function findTwoNumMax(num1, num2){
         console.log("兩個數字一樣大");
     }else{
         console.log(`最大的數字是 : ${num2}`);
-    }
-}
+    };
+};
 
 findTwoNumMax(5, 2);  // 最大的數字是 : 5
 findTwoNumMax(2, 2);  // 兩個數字一樣大
