b50fcdc35a3baf6270659c18ddb7e5c8430a1ab2
Penny Lin <ox1566@gmail.com>
2022-11-07 16:33:23 +0800

    upload max_of_two_numbers.js

---
+++ Flow_Control_Assigment/max_of_two_numbers.js
@@ -0,0 +1,13 @@
+function findTwoNumMax(num1, num2){
+    if (num1 > num2){
+        console.log(`最大的數字是 : ${num1}`);
+    }else if(num1 == num2){
+        console.log("兩個數字一樣大");
+    }else{
+        console.log(`最大的數字是 : ${num2}`);
+    }
+}
+
+findTwoNumMax(5, 2);  // 最大的數字是 : 5
+findTwoNumMax(2, 2);  // 兩個數字一樣大
+findTwoNumMax(1, 10);  // 最大的數字是 : 10
