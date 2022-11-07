4370f15dd31f0f1f962ffac3840173eb1c36b8ce
Penny Lin <ox1566@gmail.com>
2022-11-07 17:12:12 +0800

    update multiplication_table.js

--- Flow_Control_Assigment/multiplication_table.js
+++ Flow_Control_Assigment/multiplication_table.js
@@ -2,9 +2,9 @@ function multiplicationTable(){
     for(let i = 1; i < 10; i++){
         for(let j = 1; j < 10; j++){
             console.log(`${i} x ${j} = ${i * j}`);
-        }
-    }
-}
+        };
+    };
+};
 
 multiplicationTable();
 
