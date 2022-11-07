1513f829024999b389728bc79941fb14205feea8
Penny Lin <ox1566@gmail.com>
2022-11-07 16:47:45 +0800

    upload triangle.js

---
+++ Flow_Control_Assigment/triangle.js
@@ -0,0 +1,19 @@
+function triangle(size){
+    let star = "*";
+    for(let i = 1; i < size + 1; i++){
+        console.log(star);
+        star += "*";
+    };
+};
+
+triangle(5);
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
